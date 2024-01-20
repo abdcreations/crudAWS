4ad6dc22cf2dc74826e869d672421d02f9b811fd
abdcreations <abhineetdeepbksc@gmail.com>
2024-01-20 16:54:46 +0530

    Initial commit

---
+++ test/dynamo_db_crud.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamoDbCrud from '../lib/dynamo_db_crud-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamo_db_crud-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamoDbCrud.DynamoDbCrudStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
