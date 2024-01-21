4a9997cb8ebaec7720ead1b4a2d1962641f8cd8b
abdcreations <abhineetdeepbksc@gmail.com>
2024-01-21 16:47:27 +0530

    crud aws using cli

--- bin/dynamo_db_crud.ts
+++ bin/dynamo_db_crud.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+// import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core'
 import { DynamoDbCrudStack } from '../lib/dynamo_db_crud-stack';
 
 const app = new cdk.App();
--- lib/dynamo_db_crud-stack.ts
+++ lib/dynamo_db_crud-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+
 
 export class DynamoDbCrudStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB table
+    const table = new dynamodb.Table(this, 'UserTable', {
+      partitionKey: { name: "userid", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamoDbCrudQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function
+    const handler = new lambda.Function(this, 'CrudHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.main',
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Grant the Lambda function permissions to read/write to the DynamoDB table
+    table.grantReadWriteData(handler);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'CrudApi');
+
+    const integration = new apigateway.LambdaIntegration(handler);
+    const resource = api.root.addResource('user');
+    resource.addMethod('GET', integration);    // Read
+    resource.addMethod('POST', integration);   // Create
+    resource.addMethod('PUT', integration);    // Update
+    resource.addMethod('DELETE', integration); // Delete
   }
 }
+
